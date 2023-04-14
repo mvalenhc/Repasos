4fa739cdb90fc372611bdd8522c1870826bd50da
camilagv1 <mcgrajalesv01@gmai.com>
2023-04-14 17:23:56 -0500

    pagina

--- app/app.js
+++ app/app.js
@@ -3,9 +3,16 @@ import dotenv from "dotenv";
 import  LoginRoute  from "./routes/login.routes.js";
 import  passport from "passport";
 import "./middleware/google.js";
+import ejs  from "ejs";
+import path from "path";
+import * as url from "url";
+import routeHome from "./routes/backoffice.routes.js";
+import route from "./routes/home.routes.js";
 
 
 const app = express();
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 //SETTINGS
 dotenv.config();
@@ -13,6 +20,11 @@ dotenv.config();
 //MIDDLEWARES
 app.use(express.json());
 app.use(passport.initialize());
+app.use(express.static(__dirname + '../public'))
+//asignando plantilla ejs
+app.set("view engine", "ejs")
+//vistas
+app.set("views" , path.join(__dirname, "views"));
 
 app.use("/auth",passport.authenticate('auth-google',{
     scope:["https://www.googleapis.com/auth/userinfo.email",
@@ -20,12 +32,14 @@ app.use("/auth",passport.authenticate('auth-google',{
     session:false
 
 }),LoginRoute);
+app.use("/", routeHome);
+app.use("/", route)
 
 app.set("port", process.env.PORT || 6999);
 
-app.get("/",(req,res)=>{
-    res.send("Hi Welcome")
-});
+// app.get("/",(req,res)=>{
+//     res.send("Hi Welcome")
+// });
 
 
 
